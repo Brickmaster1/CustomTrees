926d19df6c1f8c7de83307317fd667ff8e6a3202
Logan <brickmasterlogan@icloud.com>
2022-09-14 21:32:47 -0400

    Creating blocks and cool stuff

---
+++ src/main/java/com/brickmasterhunt/customtrees/item/ModItems.java
@@ -0,0 +1,24 @@
+package com.brickmasterhunt.customtrees.item;
+
+import com.brickmasterhunt.customtrees.customtrees;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import javax.swing.*;
+import javax.swing.plaf.synth.Region;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, customtrees.MOD_ID);
+
+    public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
